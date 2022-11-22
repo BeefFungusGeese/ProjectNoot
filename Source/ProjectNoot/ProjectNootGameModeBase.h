33f7fd38f4f7ed5fa3dbb3b1454f92fc3f6e6a31
Angry Lizard <gitlab@netliz.net>
2022-11-22 22:00:14 +0100

    Added Unreal project for the nootz

---
+++ Source/ProjectNoot/ProjectNootGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectNootGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTNOOT_API AProjectNootGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
